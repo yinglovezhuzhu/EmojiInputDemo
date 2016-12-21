717e79d45ffd1ad00af803dd14d8f4088c3a8f80
yinglovezhuzhu <yinglovezhuzhu@139.com>
2016-12-21 16:38:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/facedemo/FileUtils.java
@@ -0,0 +1,40 @@
+package com.example.facedemo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+
+/**
+ * 文件工具类
+ * @author yinglovezhuzhu@gmail.com
+ */
+public class FileUtils {
+	/**
+	 * 读取表情配置文件
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static List<String> getEmojiFile(Context context) {
+		try {
+			List<String> list = new ArrayList<String>();
+			InputStream in = context.getResources().getAssets().open("emoji");// �ļ�����Ϊrose.txt
+			BufferedReader br = new BufferedReader(new InputStreamReader(in,
+					"UTF-8"));
+			String str = null;
+			while ((str = br.readLine()) != null) {
+				list.add(str);
+			}
+
+			return list;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
